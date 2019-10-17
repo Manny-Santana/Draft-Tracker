7fc626506420d02450af3be8ee6b99df9973b6ce
Manny-Santana <aresxwarlord@outlook.com>
2019-10-17 11:16:47 -0400

    successfully added player to board with relevant stats

--- src/app.js
+++ src/app.js
@@ -15,9 +15,22 @@ const callAPI = (url, callback) => {
     callback(data);
   });
 };
-const findPlayerbyName = (playerObjectArray, playerName) => {
+const findPlayerID = (players, playerName) => {
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].Name.toLowerCase() === playerName.toLowerCase()) {
+      console.log("found player ID by name");
+      console.log(`Name: ${players[i].Name} \n
+        PlayerID: ${players[i].PlayerID}`);
+      // return playerObjectArray[i].PlayerID;
+      //return instead player object
+      return players[i].PlayerID;
+    }
+  }
+};
+const findPlayerByName = (playerObjectArray, playerName) => {
   for (let i = 0; i < playerObjectArray.length; i++) {
     if (playerObjectArray[i].Name.toLowerCase() === playerName.toLowerCase()) {
+      console.log("found player object by name");
       console.log(`Name: ${playerObjectArray[i].Name} \n
         PlayerID: ${playerObjectArray[i].PlayerID}`);
       // return playerObjectArray[i].PlayerID;
@@ -39,35 +52,93 @@ const getTeams = "https://api.sportsdata.io/v3/nfl/scores/json/Teams/2019REG";
 
 const playerWeek =
   "https://api.sportsdata.io/v3/nfl/projections/json/DfsSlatesByWeek/2019REG/1";
+const getPlayerStatsByWeek = (playersArray, playername, week) => {
+  const playerid = findPlayerID(playersArray, playername);
+  const statsURL = `https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByPlayerID/2019REG/${week}/${playerid}`;
+};
+const getSeasonStatsByPlayer = playerID => {
+  const seasonStatsByPlayer = `https://api.sportsdata.io/v3/nfl/stats/json/PlayerSeasonStatsByPlayerID/2019REG/${playerID}`;
+  return seasonStatsByPlayer;
+};
 
-//creates each
-const createPlayerComponent = player => {
+//creates each player card by combining api call data
+//player - should be each individual player object likely passed via looping the players array
+//statsforgivenweek  - should be obtained from calling the api on the playerstats per week endpoint
+
+const createPlayerComponent = (player, statsforseason) => {
   const $img = $("<img>");
   $img.attr("src", getImage(player)); //set the players image
   const $row = $("<tr>").addClass("player-data");
-  const tdPlayerCard = $("<td>").addClass("player-card");
-  const h4Name = $("<h4>").text(player.Name);
-  // <tr class="player-data">
-  //               <td class='player-card'>
-  //               </h4>Antonio Allison
-  //                   <h4>
+  //TODO
+  const fantasypoints = statsforseason[0].FantasyPoints;
+  const gamesPlayed = statsforseason[0].Played;
+  //get fantasypoints somehow
+  const targets = statsforseason[0].ReceivingTargets;
+  const rushattempts = statsforseason[0].RushingAttempts;
 
-  //                   <img src="images/nfl-arizona-cardinals-team-logo-2-768x768.png" alt=""></td>
-  //               <td>Green Bay Packers</td>
-  //               <td>WR</td>
-  //               <td>stat1</td>
-  //               <td>stat1</td>
+  // const tdPlayerCard = $("<td>").addClass("player-card");
+  // const h4Name = $("<h4>").text(player.Name);
+  $row.html(
+    `
+    
+                  <td class='player-card'>
+                  </h4>${player.Name}<h4>
+  
+                      <img src=${player.PhotoUrl} alt="player"></td>
 
-  //           </tr>
+                  <td>${player.Team}</td>
+                  <td>${player.Position}</td>
+                  <td> <h4>AVERGAE Fantasy Points PER GAME: ${fantasypoints /
+                    gamesPlayed} </h4></td>
+                  <td><h4>Targets: ${targets + rushattempts} </h4></td>
+    `
+  );
+  $("tbody").append($row);
 };
 
 $(() => {
-  // callAPI(getPlayerData, players => {
-  //   const img = $("<img>").attr(
-  //     "src",//findPlayerID was changed to findPlayerByName
-  //     findPlayerID(players, "David Johnson").PhotoUrl
-  //   );
-  //   $(".results").append(img);
-  //   // console.log(findPlayerID(players, "David Johnson").PhotoUrl);
+  //GET PLAYER STATS BY SEASON!!!
+  // callAPI(seasonStatsByPlayer, player => {
+  //   console.log(player);
   // });
+  // console.log("connected");
+  //GET PLAYER OBJECT BY NAME AND GET PLAYER ID BY NAME
+  //WORKS!!!
+  callAPI(getPlayerData, players => {
+    const player = findPlayerByName(players, "James Conner");
+    callAPI(getSeasonStatsByPlayer(player.PlayerID), seasondata => {
+      createPlayerComponent(player, seasondata);
+    });
+  });
+  // callAPI(playerGameStatsByPlayer, player => {});
+  // $.ajax({
+  //   type: "GET",
+  //   url: `https://api.sportsdata.io/v3/nfl/stats/json/PlayerGameStatsByPlayerID/2019REG/1/${findPlayerID(
+  //     players,
+  //     "James Conner"
+  //   )}`,
+  //   dataType: "json",
+  //   headers: {
+  //     "Ocp-Apim-Subscription-Key": key
+  //   },
+  //   success: function() {
+  //     alert("authentication successful");
+  //   }
+  // }).done(data => {
+  //   console.log(data);
+  // });
+  // let players = $.ajax({
+  //   type: "GET",
+  //   url: getPlayerData,
+  //   dataType: "json",
+  //   headers: {
+  //     "Ocp-Apim-Subscription-Key": key
+  //   },
+  //   success: function() {
+  //     alert("authentication successful");
+  //   }
+  // }).done(data => {
+  //   console.log(data);
+  // });
+  // console.log(`all players: ${players}`);
 });
